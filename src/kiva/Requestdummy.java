21719ae2e48366c2036a341046a52b395f26e1b8
Christophe Quignon <cuignon@gmail.com>
2014-10-29 16:44:06 +0100

    spawning recipient agents

--- src/kiva/Requestdummy.java
+++ src/kiva/Requestdummy.java
@@ -1,6 +1,9 @@
 package src.kiva;
 
 import jade.core.Agent;
+import jade.wrapper.AgentContainer;
+import jade.wrapper.StaleProxyException;
+
 import java.util.Random;
 
 /**
@@ -9,56 +12,54 @@ import java.util.Random;
 public class Requestdummy extends Agent {
 
 	protected void setup() {
-		
-		//say hello
-		System.out.println("request dummy is here");
-		
-		/** **/
-		//spawn recipient
-		while (true){
-			
-			Random rand = new Random();
-			
-			
-			String order = " ";
-			
-			//choose a random number of parts
-			for( int parts = 0; parts<rand.nextInt(12); parts++){
 
-				//The part name is a string of a random number
+		// say hello
+		System.out.println("request dummy is here");
+
+		/** **/
+		// spawn recipient
+		while (true) {
+
+			Random rand = new Random();
+
+			String order = " ";
+
+			// choose a random number of parts
+			for (int parts = 0; parts < rand.nextInt(12); parts++) {
+
+				// The part name is a string of a random number
 				int random = rand.nextInt(1000);
 				String product = Integer.toString(random);
-				
-				//Order the  part at most 12 times
 
-				for(int amount = 0 ; amount < rand.nextInt(13); amount++){
+				// Order the part at most 12 times
+
+				for (int amount = 0; amount < rand.nextInt(13); amount++) {
 					order = order + product + ", ";
 				}
 
 			}
-			
-			//TODO: publish order
-			System.out.println(order);
-			
 
-			//wait some time
+			// spawn agent
+			AgentContainer container = getContainerController();
+			try {
+				container.createNewAgent("rec:" + order, "src.kiva.Recipient",
+						null);
+			} catch (StaleProxyException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+
+			// TODO: publish order
+			System.out.println(order);
+
+			// wait some time
 			try {
 				Thread.sleep(rand.nextInt(5000));
 			} catch (InterruptedException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-			
-			
-			
+
 		}
 	}
-	/** 
-	public class SpawnOrder extends CyclicBehaviour{
-		public void action{
-			
-		}
-		
-	}
-	**/
 }
