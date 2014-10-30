94e9062be387c61d0242420eae47ce12aef870be
Christophe Quignon <cuignon@gmail.com>
2014-10-30 15:56:03 +0100

    argument to dummy order agent added

--- src/kiva/Requestdummy.java
+++ src/kiva/Requestdummy.java
@@ -11,13 +11,32 @@ import java.util.Random;
  */
 public class Requestdummy extends Agent {
 
+	private int max_parts_per_order = 12;
+	private int max_amount_per_part = 12;
+	private double max_sec_delay = 5;
+
 	protected void setup() {
 
 		// say hello
 		System.out.println("request dummy is here");
 
-		/** **/
+		Object[] args = getArguments();
+		if (args != null && args.length > 2) {
+			max_parts_per_order = Integer.parseInt(args[0].toString());
+			max_amount_per_part = Integer.parseInt(args[1].toString());
+			max_sec_delay = Double.parseDouble(args[2].toString());
+		}
+
 		// spawn recipient
+		AgentContainer container = getContainerController();
+		try {
+			container.createNewAgent("recipient", "src.kiva.Recipient", null);
+		} catch (StaleProxyException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+
+		// generate orders
 		while (true) {
 
 			Random rand = new Random();
@@ -25,7 +44,7 @@ public class Requestdummy extends Agent {
 			String order = " ";
 
 			// choose a random number of parts
-			for (int parts = 0; parts < rand.nextInt(12); parts++) {
+			for (int parts = 0; parts < rand.nextInt(max_parts_per_order); parts++) {
 
 				// The part name is a string of a random number
 				int random = rand.nextInt(1000);
@@ -33,20 +52,10 @@ public class Requestdummy extends Agent {
 
 				// Order the part at most 12 times
 
-				for (int amount = 0; amount < rand.nextInt(13); amount++) {
+				for (int amount = 0; amount < rand.nextInt(max_amount_per_part); amount++) {
 					order = order + product + ", ";
 				}
 
-			}
-
-			// spawn agent
-			AgentContainer container = getContainerController();
-			try {
-				container.createNewAgent("rec:" + order, "src.kiva.Recipient",
-						null);
-			} catch (StaleProxyException e1) {
-				// TODO Auto-generated catch block
-				e1.printStackTrace();
 			}
 
 			// TODO: publish order
@@ -54,7 +63,7 @@ public class Requestdummy extends Agent {
 
 			// wait some time
 			try {
-				Thread.sleep(rand.nextInt(5000));
+				Thread.sleep(rand.nextInt((int) (max_sec_delay * 1000)));
 			} catch (InterruptedException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
@@ -62,4 +71,5 @@ public class Requestdummy extends Agent {
 
 		}
 	}
+
 }
