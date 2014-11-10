c21107c28cbe74aeda65bf259b77c566ec1e003b
Christophe Quignon <cuignon@gmail.com>
2014-11-10 11:10:53 +0100

    Broadcast via DFService 'giveOrder' implemented.

--- src/kiva/Order.java
+++ src/kiva/Order.java
@@ -1,6 +1,10 @@
 package src.kiva;
 
 import jade.core.Agent;
+import jade.domain.DFService;
+import jade.domain.FIPAException;
+import jade.domain.FIPAAgentManagement.DFAgentDescription;
+import jade.domain.FIPAAgentManagement.ServiceDescription;
 
 /**
  * The order agents contains the order and answers the picker agent
@@ -19,8 +23,21 @@ public class Order extends Agent {
 				// System.out.println(order[i]);
 			}
 		}
-	}
 
+		// register "giveorder" Service
+		DFAgentDescription dfd = new DFAgentDescription();
+		dfd.setName(getAID());
+		ServiceDescription sd = new ServiceDescription();
+		sd.setType("giveOrder");
+		sd.setName("kivaOrder");
+		dfd.addServices(sd);
+		try {
+			DFService.register(this, dfd);
+		} catch (FIPAException fe) {
+			fe.printStackTrace();
+		}
+	}
+	
 	// TODO: Answer
 
 }
--- src/kiva/Picker.java
+++ src/kiva/Picker.java
@@ -1,14 +1,63 @@
 package src.kiva;
 
+import jade.core.AID;
 import jade.core.Agent;
+import jade.core.ServiceDescriptor;
+import jade.core.behaviours.CyclicBehaviour;
+import jade.domain.DFService;
+import jade.domain.FIPAException;
+import jade.domain.FIPAAgentManagement.DFAgentDescription;
+import jade.domain.FIPAAgentManagement.ServiceDescription;
+import jade.lang.acl.ACLMessage;
 
 /**
  * The picker agent requests orders. From these orders, it requests the
  * products. Afterwards it returns the order.
  **/
 public class Picker extends Agent {
+	private String[] orderAgents;
+	private String[] order;
+	private Boolean isBusy;
+	private	AID[] sellerAgents;
 
 	protected void setup() {
+		isBusy = false;
+
+		addBehaviour(new CyclicBehaviour(this) {
+			public void action() {
+				if (!isBusy) {
+					
+					// get all agents with a "giveOrder" service
+					DFAgentDescription template = new DFAgentDescription();
+					ServiceDescription sd = new ServiceDescription();
+					sd.setType("giveOrder");
+					template.addServices(sd);
+					try {
+						DFAgentDescription[] result = DFService.search(myAgent,
+								template);
+						sellerAgents = new AID[result.length];
+						for (int i = 0; i < result.length; ++i) {
+							sellerAgents[i] = result[i].getName();
+						}
+					} catch (FIPAException fe) {
+						fe.printStackTrace();
+					}
+					
+					// send message
+					for(AID agent:sellerAgents){
+						ACLMessage msg = new ACLMessage(ACLMessage.INFORM);
+						msg.setContent("Do you have an order for me? ");
+						msg.addReceiver(agent);
+						send(msg);
+						//System.out.println(msg.getContent() + agent.getName());
+					}
+					
+					//TODO: wait for answer
+					isBusy = true;
+					
+				}
+			}
+		});
 
 	}
 
