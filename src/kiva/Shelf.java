6193668c696c089c12d254d9ebde1cd6c6198ea2
menaka <menaka@menaka-laptop.(none)>
2014-11-18 15:47:01 +0100

    Implementing Shelves

--- src/kiva/Shelf.java
+++ src/kiva/Shelf.java
@@ -2,13 +2,27 @@ package src.kiva;
 
 import jade.core.Agent;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * The shelf keep yet specified how a shelf is refilled.
  **/
 public class Shelf extends Agent {
 
 	protected void setup() {
-
+		Map<String,Integer> shelves = new HashMap<String,Integer>();
+		shelves.put("0", 10);
+		shelves.put("1", 10);
+		shelves.put("2", 10);
+		shelves.put("3", 10);
+		shelves.put("4", 10);
+		shelves.put("5", 10);
+		shelves.put("6", 10);
+		shelves.put("7", 10);
+		shelves.put("8", 10);
+		shelves.put("9", 10);
+		
 	}
 
 }
